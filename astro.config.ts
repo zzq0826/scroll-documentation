14da8c7f97f701305bf33426f83dbc17a01d9b27
zzq0826 <12936800+zzq0826@users.noreply.github.com>
2025-11-21 10:00:07 +0800

    rest

--- astro.config.ts
+++ astro.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
     remarkPlugins: [remarkMath, remarkGfm],
     rehypePlugins: [
       rehypeSlug,
-      [rehypeMermaid, { strategy: "preview" }],
+      [rehypeMermaid, { strategy: "pre-mermaid" }],
       [
         rehypeAutolinkHeadings,
         {
